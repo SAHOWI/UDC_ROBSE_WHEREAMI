feea0b05d99ce2184b4f85630bbda780d0d073cb
SAHOWI <wienecke.ch@gmail.com>
2021-04-02 17:19:35 +0200

    [MOD] completed process_image

--- catkin_ws/src/ball_chaser/src/process_image.cpp
+++ catkin_ws/src/ball_chaser/src/process_image.cpp
@@ -8,19 +8,64 @@ ros::ServiceClient client;
 // This function calls the command_robot service to drive the robot in the specified direction
 void drive_robot(float lin_x, float ang_z)
 {
-    // TODO: Request a service and pass the velocities to it to drive the robot
+    // Request a service and pass the velocities to it to drive the robot
+    ball_chaser::DriveToTarget drive_cmd;
+    drive_cmd.request.linear_x = lin_x;
+    drive_cmd.request.angular_z = ang_z;
+    
+    if (!client.call(drive_cmd)){
+        ROS_ERROR("Failed to execute drive command");
+    }
 }
 
 // This callback function continuously executes and reads the image data
 void process_image_callback(const sensor_msgs::Image img)
 {
 
-    int white_pixel = 255;
-
-    // TODO: Loop through each pixel in the image and check if there's a bright white one
+    // Loop through each pixel in the image and check if there's a bright white one
     // Then, identify if this pixel falls in the left, mid, or right side of the image
     // Depending on the white ball position, call the drive_bot function and pass velocities to it
     // Request a stop when there's no white ball seen by the camera
+    
+    // Definitions
+    int white_pixel = 255;
+    int white_moment = 0;
+    int white_pixels = 0;
+    int total_pixels = 0;
+    float awm;
+    float x, z;
+ 
+    for(int i=0; i<img.height; ++i)
+    {
+        for (int j=0; j<img.step; ++j)
+        {
+            ++total_pixels;
+            
+            if (img.data[i * img.step + j] == white_pixel_val)
+            {
+                ++white_pixels;
+                white_moment = white_moment + j - img.step/2;
+                awm = white_moment/white_pixels;
+            }
+        }
+    }
+    
+    ROS_INFO("%d white pixels found - awm is:  %1.2f ", (int)white_pixels,awm);
+
+    if (white_pixels == 0) 
+    { // no pixels found turn until you find some!
+        x = 0.0;
+        z = -.5;
+    }  
+    else 
+    {
+        x = 0.1;
+        z=  -0.5*awm/600;
+    }
+    
+    // next move
+    drive_robot(x,z);
+
 }
 
 int main(int argc, char** argv)
