4f2246b23c459da6ef77a2aa9ae7494b8459ecf9
SAHOWI <wienecke.ch@gmail.com>
2021-04-05 12:48:27 +0000

    [MOD] Changes made based on Review #2

--- catkin_ws/src/ball_chaser/src/process_image.cpp
+++ catkin_ws/src/ball_chaser/src/process_image.cpp
@@ -28,6 +28,8 @@ void process_image_callback(const sensor_msgs::Image img)
     // Request a stop when there's no white ball seen by the camera
     
     // Definitions
+    //  old version to be corrected 
+    /*
     int white_pixel_val = 255;
     int white_moment = 0;
     int white_pixels = 0;
@@ -65,8 +67,60 @@ void process_image_callback(const sensor_msgs::Image img)
     
     // next move
     drive_robot(x,z);
+*/
 
+
+// Complete rework of the initial Version, as the initial version (allthough working) was not considering the 3 RGB channels seperately
+// This rework is based on Review Feedback and related on knowledge artikel  https://knowledge.udacity.com/questions/368996
+// Assumption: The white Ball is level with the camera, therefore we only have to scan the middle third of the image
+  int scan_s = img.data.size() / 3;
+  int scan_e = img.data.size() * 2 / 3;
+
+  int num_white_pixels = 0;
+  int x_position_sum = 0;
+
+  // Scan each pixel looking for a white one
+  for (int i=scan_s; i+2<scan_e; i+=3) {
+
+
+    // as we have a RGB model, we will have at position p the red channel, p+1 the green channel and p+2 the blue channel.
+    // only if all three values are at level 255 we can assume pure white (FF:FF:FF)
+
+    int red_channel = img.data[i];
+    int green_channel = img.data[i+1];
+    int blue_channel = img.data[i+2];
+
+    if (red_channel == 255 && green_channel == 255 && blue_channel == 255)
+    {
+      int x_position = (i % (img.width * 3)) / 3;
+      x_position_sum += x_position;
+      num_white_pixels += 1;
+    }
+  }
+
+  if (num_white_pixels == 0)
+  {
+    // no pixels found turn until you find some!
+        drive_robot(0.0, -0.5);
+  }
+  else
+  {
+    int mean_x_position = x_position_sum / num_white_pixels;
+    if (mean_x_position < img.width / 3)
+    {
+      drive_robot(0.5, 0.5);
+    }
+    else if (mean_x_position > img.width * 2 / 3)
+    {
+      drive_robot(0.5, -0.5);
+    }
+    else
+    {
+      drive_robot(0.5, 0.0);
+    }
+  }
 }
+
 
 int main(int argc, char** argv)
 {
